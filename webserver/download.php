e437a2aeadca252beee91010f7890c5e29b3a5e8
David <pre@PRENAS.(none)>
2016-04-08 21:58:47 -0700

    added webserver

---
+++ webserver/download.php
@@ -0,0 +1,14 @@
+<?php
+echo "Executing Script with inputs:";
+
+$YTLINK = htmlspecialchars($_POST["ytlink"]);
+$FOLDER = escapeshellarg(htmlspecialchars($_POST["foldername"]));
+
+echo "Youtube Link: " . $YTLINK . "\r\n";
+echo "Folder Name: " . $FOLDER . "\r\n";
+
+echo "/media/USBHDD1/YOUTUBE_DL/DL_SCRIPT/youtube-dl.sh $YTLINK $FOLDER ";
+$output = shell_exec("/media/USBHDD1/YOUTUBE_DL/DL_SCRIPT/youtube-dl.sh " . $YTLINK . " " . $FOLDER);
+echo "$output";
+
+?>
