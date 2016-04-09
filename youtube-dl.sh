60c2204169cc700e1e408326db0e7f0bf097c963
David <pre@PRENAS.(none)>
2016-04-08 21:57:31 -0700

    added script

---
+++ youtube-dl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##################
+#ARGUMENTS:
+#1: Playlist Link
+#2: Directory Name (optional)
+##################
+
+dirname="/media/USBHDD1/YOUTUBE_DL/dl"
+if [ "$1" != "" ]; then
+    echo "Processing the following link:" $1
+    if [ "$2" != "" ]; then
+        dirname=$dirname"_"$2"/"
+    else
+        dirname=$dirname"/"
+    fi
+    
+    echo "Directory: " $dirname
+    if [ ! -d "$dirname" ]; then
+        echo Creating directory: $dirname
+        mkdir -p -- "$dirname"
+    fi
+    screen -dmS download youtube-dl -x --audio-format "mp3" --restrict-filenames -i -o "$dirname%(title)s.%(ext)s" $1 
+else
+    echo "No Input, enter following"
+    echo "1: Playlist Link"
+    echo "2: Folder Name (optional)"
+fi
